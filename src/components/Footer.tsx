d4895807f1ca7b54807bb091579a2f5ebb157ef1
Agung Maulana <iamaul@hotmail.com>
2021-04-13 05:17:23 +0700

    add: credits dev.prineside.com on Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,10 @@ import NextLink from 'next/link';
 
 export const Footer = () => (
   <Stack my={'3rem'} align="center" spacing={4} maxW="4xl" mx="auto">
-    <Text fontSize="smaller">
+    <Text fontSize='smaller'>
+        Texture images from dev.prineside.com
+    </Text>
+    <Text fontSize="xs">
       Made with ❤️ by <NextLink href="https://iamaul.me" passHref><Link href="https://iamaul.me" target="_blank">iamaul</Link></NextLink>
     </Text>
   </Stack>
