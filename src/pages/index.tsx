c2ef5eaa5805b889ae0526a6e6943fa0a555f0ad
Agung Maulana <iamaul@hotmail.com>
2021-04-13 05:08:56 +0700

    add: site icons and manifest.json

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps, InferGetStaticPropsType } from 'next';
 import { NextSeo } from 'next-seo';
+import Head from 'next/head';
 
 import { Hero } from '@/components/Hero';
 import { Container } from '@/components/Container';
@@ -15,6 +16,24 @@ import siteConfig from '~/site.config';
 const Index = ({ textures }: InferGetStaticPropsType<typeof getStaticProps>) => (
   <>
     <NextSeo title={siteConfig.title} titleTemplate='%s' />
+    <Head>
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <title>SA-MP Textures</title>
+      <link rel="manifest" href="/site.webmanifest.json" />
+      <link
+        href="/android-chrome-192x192.png"
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+      />
+      <link
+        href="/android-chrome-512x512.png"
+        rel="icon"
+        type="image/png"
+        sizes="512x512"
+      />
+      <link rel="apple-touch-icon" href="/apple-icon.png" />
+    </Head>
     <Container>
       <Hero />
       <ToggleColorMode />
